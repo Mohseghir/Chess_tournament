3aef21de805e48020db7b94750052cf1d12abe1c
Mohseghir <baghaoui@homtail.com>
2022-12-27 10:59:16 +0100

    creation du fichier json

--- Player.py
+++ Player.py
@@ -1,39 +1,65 @@
+import json
+
+
 # player model
+
+
 class Player:
     """model pour un joueur"""
-    def __init__(self, last_name, first_name, birthday, gender, player_id, ranking: int, score=0):
+
+    def __init__(self, last_name, first_name, birthday,
+                 gender, player_id, ranking, score):
         """initialiser un joueur"""
         self.last_name = last_name
         self.first_name = first_name
         self.birthday = birthday
         self.gender = gender
         self.player_id = player_id
-        self.ranking = int(ranking)
+        self.ranking = ranking
         self.score = score
 
     def __str__(self):
-        """representer un joueur"""
-        return f"{self.last_name} le {self.first_name}"
+        """format lisible de la class"""
+        return f"Nom : {self.last_name}, Prénom :  {self.first_name}"
 
     def __repr__(self):
-        """Représentation de l'objet"""
-        return f"les informations du joueur sont :  {self.last_name}, {self.first_name}"
+        return str(self)
 
+    @staticmethod
+    def player_serialisation(self):
+        """sérialiser les info d'un joueur avant utilisation avec json"""
+        self.serialised_player = {
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "birthday": self.birthday,
+            "gender": self.gender,
+            "player_id": self.player_id,
+            "ranking": self.ranking,
+            "score": self.score
+        }
+        return self.serialised_player
 
-# player view
 
 class PlayerView:
-    @classmethod
-    def add_player(self):
+    @staticmethod
+    def add_player():
         """saisis des informations d'un joueur """
         last_name = input("Nom : ")
         first_name = input("Prènom : ")
         birthday = input("Date de naissance sous la forme JJ/MM/AAAA : ")
         gender = input("Genre M / F : ")
+        player_id = input("ID du joueur : ")
         ranking = input("Classement : ")
         score = input("Score : ")
         return Player(last_name=last_name, first_name=first_name, birthday=birthday,
-                      gender=gender, ranking=ranking, score=score)
+                      gender=gender, player_id=player_id, ranking=int(ranking), score=int(score))
 
-# controler
 
+player = Player("baghaoui", "moha", "29/10/1989", "M", "ERDFE", 4, 2)
+
+data = player.player_serialisation(player)
+
+with open("data_joueur.json", "w") as write_file:
+    json.dump(data, write_file, indent=2)
+
+print(player)
