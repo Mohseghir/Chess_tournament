41bde5e2a1ae0be94ec4632282528da0a792c659
Mohseghir <baghaoui@homtail.com>
2023-01-03 09:24:56 +0100

    réorganisation en MVC + tournamentview

---
+++ models/Tournament.py
@@ -0,0 +1,26 @@
+
+class Tournament:
+
+    def __init__(self, name, place, start_date, end_date, number_of_rounds, current_round_number,
+                 rounds_list, time_controller, description):
+        self.name = name
+        self.place = place
+        self.start_date = start_date
+        self.end_date = end_date
+        self.number_of_rounds = number_of_rounds
+        self.current_round_number = current_round_number
+        self.players_list = players_list
+        self.rounds_list = rounds_list
+        self.time_controller = time_controller
+        self.description = description
+
+    def __str__(self):
+        """format lisible de la class"""
+        return f"Le tournois {self.name} à {self.first_name}"
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def tournament_serialisation(self):
+        return self.__dict__
--- views/PlayerView.py
+++ views/PlayerView.py
@@ -1,11 +1,11 @@
-from Player import Player
+from models.Player import Player
 
 
 class PlayerView:
     @staticmethod
     def add_player():
         """saisis des informations d'un joueur
-         ECRIR UNE fct pour afficher la saisi et------------
+         ECRIR UNE fct pour afficher la saisi et
          demander confirmation ou reprise de saisir"""
         last_name = input("Nom : ")
         first_name = input("Prènom : ")
@@ -13,13 +13,11 @@ class PlayerView:
         gender = input("Genre M / F : ")
         player_id = input("ID du joueur : ")
         ranking = input("Classement : ")
-        score = input("Score : ")
+        score = 0
         return Player(last_name=last_name, first_name=first_name, birthday=birthday,
                       gender=gender, player_id=player_id, ranking=int(ranking), score=int(score))
 
     @staticmethod
-    def num_of_participant():
+    def num_of_participant():  # nbr par defaut 8
         participant_count = int(input("veulliez saisir un nombre paire de participant  : "))
         return participant_count
-
-
