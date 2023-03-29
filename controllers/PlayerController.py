68858dd0285992b6e250ad0e82c279ad76337c05
Mohseghir <baghaoui@homtail.com>
2023-03-29 12:57:05 +0100

    Round 2 played

--- controllers/PlayerController.py
+++ controllers/PlayerController.py
@@ -1,25 +1,35 @@
 import datetime
 import random
 
+# Import the classes from the models module
+
 from models.Player import Player
 from models.Match import Match
 from models.Round import Round
 from models.Tournament import Tournament
 import json
 
-participants = 2
 
+participants = 8
+
+# This function is used to create players and store them in a JSON file
 
 def create_player():
     global participants
+# Allow user to change the default number of players (8)
+
     choix = input("le numbre de joueurs par defaut est de 8, voulez vous le changer ? : n/y")
     if choix == "y":
         participants = int(input("combien de joueurs voulez-vous ajouter à la base de données ?  : "))
+# Try to load existing players from the players.json file
+
     try:
         with open('players.json', 'r') as f:
             players = json.load(f)
     except json.decoder.JSONDecodeError:
+# If the file is empty or not found, create an empty list
         players = {"player": []}
+# Prompt user to enter information for each player and append the player to the list of players
 
     for i in range(0, participants):
         player = {}
@@ -32,17 +42,24 @@ def create_player():
 
         players["player"].append(player)
         print(players)
+# Save the updated list of players to the players.json file
 
     with open('players.json', 'w') as f:
         json.dump(players, f)
 
+# This function is used to create the first round of matches
 
 def create_round_1():
     try:
+        # Try to load players from the players.json file
+
         with open('players.json', 'r') as f:
             players = json.load(f)
     except json.decoder.JSONDecodeError:
+        # If the file is empty or not found, print an error message and return
         print("problème au niveau de la lecture du fichier players.json")
+
+# Convert player data into Player objects and randomly select 8 players for the first round
 
     converted_players = [Player(player_data['last_name'],
                                 player_data['first_name'],
@@ -50,29 +67,26 @@ def create_round_1():
                                 ) for player_data in
                          players["player"]]
 
-    # players_tuple = [(player.get_first_name(),
-    #                   player.get_last_name(),
-    #                   player.get_birthday()
-    #                   ) for player in
-    #                  converted_players]
-    #
-    # print(players_tuple)
-    #
-    # random_players_for_match = random.sample(players_tuple, 2)
-    # print(random_players_for_match)
 
-    # premier round, mélange de tous les joueurs de façon aléatoire.
+# premier round, mélange de tous les joueurs de façon aléatoire.
     converted_players = random.sample(converted_players, 8)
 
     i = 0
+# Create a Round object with the round number, name, start date, and end date
+
     round = Round(1, "Round1", "12/12/1212", "13/12/1212")
     for i in range(i, 4):
+        # Select two random players from the list of converted players and create a Match object with them
+
         two_random_player_for_match = random.sample(converted_players, 2)
         match = Match(two_random_player_for_match[0],
                       two_random_player_for_match[1])
+        # Add the Match object to the Round object
+
         round.add_match(match)
 
-        # print(two_random_player_for_match[0],two_random_player_for_match[1])
+        # Print the match and remove the two players from the list of converted players
+
         print("Taille de la liste = ", len(converted_players))
 
         print(round.get_match_list()[i].get_match()[0][0],
@@ -127,8 +141,10 @@ def create_round_2(round):
     list_joueurs = []
 
     for i in range(i, 4):
-        joueur_score_1 = [round.get_match_list()[i].get_match()[0][0].get_last_name(), round.get_match_list()[i].get_match()[0][1]]
-        joueur_score_2 = [round.get_match_list()[i].get_match()[1][0].get_last_name(), round.get_match_list()[i].get_match()[1][1]]
+        joueur_score_1 = [round.get_match_list()[i].get_match()[0][0].get_last_name(),
+                          round.get_match_list()[i].get_match()[0][1]]
+        joueur_score_2 = [round.get_match_list()[i].get_match()[1][0].get_last_name(),
+                          round.get_match_list()[i].get_match()[1][1]]
         list_joueurs.append(joueur_score_1)
         list_joueurs.append(joueur_score_2)
 
@@ -136,7 +152,36 @@ def create_round_2(round):
     sorted_players = sorted(list_joueurs, key=lambda x: x[1], reverse=True)
     print(sorted_players)
 
-    # list_joueurs_tri
+    round = Round(2, "Round2", "14/12/1212", "15/12/1212")
+    for i in range(0, len(sorted_players), 2):
+        print("{} vs. {}".format(sorted_players[i], sorted_players[i + 1]))
+        verify = has_played(sorted_players[i], sorted_players[i + 1], round)
+        print(verify)
+        if not has_played(sorted_players[i], sorted_players[i + 1], round):
+            match = Match(sorted_players[i], sorted_players[i + 1])
+            round.add_match(match)
+        else:
+            match = Match(sorted_players[i], sorted_players[i + 2])
+            round.add_match(match)
+
+    for i in range(0, 4):
+        print(round.get_match_list()[i].get_match()[0][0],
+              "",
+              round.get_match_list()[i].get_match()[0][1],
+              " -#- ",
+              round.get_match_list()[i].get_match()[1][0],
+              "",
+              round.get_match_list()[i].get_match()[1][1])
+
+
+def has_played(player_1, player_2, round):
+    for match in round.get_match_list():
+        if (player_1 in match.get_match()[0] or player_1 in match.get_match()[1]
+        ) and (player_2 in match.get_match()[0] or player_2 in match.get_match()[1]):
+            #print("True")
+            return True
+    #print("False")
+    return False
 
 
 def create_tournament():
